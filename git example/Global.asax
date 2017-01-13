ec8879736e2cef77668ed4742bddc3e88aea1e2c
dearthm <dearthml@gmail.com>
2017-01-13 14:35:38 -0500

    Add project files.

---
+++ git example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="git_example.Global" Language="C#" %>
---
+++ git example/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git_example.Startup))]
+namespace git_example
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
